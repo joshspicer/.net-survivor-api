fb1d4815a12e09206fd36fcfd2d8db1012deada9
Josh Spicer <hello@joshspicer.com>
2019-05-26 14:55:06 -0400

    Add project files.

---
+++ survivorapi2/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace survivorapi2.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ survivorapi2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="survivorapi2.WebApiApplication" Language="C#" %>
