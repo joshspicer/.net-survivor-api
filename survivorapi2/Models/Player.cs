cc6ca2616d51f701b80d5990d5c330ba63d6ae70
Josh Spicer <hello@joshspicer.com>
2019-05-27 13:38:16 -0400

    Defined data model

---
+++ survivorapi2/Models/Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Web;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace survivorapi2.Models
+{
+    public class Player
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Year { get; set; }
+        public string Major { get; set; }
+        public string Hometown { get; set; }
+
+    }
+}
---
+++ survivorapi2/Models/Tribe.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace survivorapi2.Models
+{
+    public class Tribe
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Color { get; set; }
+
+
+        // Members of the tribe
+        public Player[] players { get; set; }
+
+    }
+}
